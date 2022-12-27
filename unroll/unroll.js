159e5678cc44fa7def6eebb2439fd0a06fb79b50
Nick Fulghum <nickfulghum92@gmail.com>
2022-12-27 10:54:29 -0600

    unroll completed

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,5 +1,35 @@
-function unroll(squareArray) {
+const square = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+];
 
+
+const unroll = (squareArray, arr = []) => {
+    for (let i = 0; i < squareArray.length; i++) {
+        arr.push(squareArray[0][i])
+    }
+    squareArray.shift();
+
+    for (let j = 0; j < squareArray.length; j++) {
+        arr.push(squareArray[j][squareArray.length])
+        squareArray[j].pop()
+    }
+    for (let k = squareArray.length - 1; k >= 0; k--) {
+        arr.push(squareArray[squareArray.length - 1][k])
+    }
+    squareArray.pop();
+
+    for (let l = squareArray.length - 1; l >= 0; l--) {
+        arr.push(squareArray[l][0])
+        squareArray[l].shift()
+    }
+
+    if (squareArray.length > 0) {
+        return unroll(squareArray, arr)
+    }
+    return arr
 }
 
 module.exports = unroll;
