7fb8fd079b6acb992e6e6f7cbd512529080148ab
Nick Fulghum <nickfulghum92@gmail.com>
2023-01-07 09:32:15 -0600

    updated comments

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -7,25 +7,36 @@ const square = [
 
 
 const unroll = (squareArray, arr = []) => {
+    // remove the first row from the array and place in new array and refactor squareArray
+    // 1,2,3,4 // 6, 7
     for (let i = 0; i < squareArray.length; i++) {
         arr.push(squareArray[0][i])
     }
     squareArray.shift();
 
+    // loop through the array and remove the last element from each row in the array
+    // 8,12,16, 11
     for (let j = 0; j < squareArray.length; j++) {
         arr.push(squareArray[j][squareArray.length])
         squareArray[j].pop()
     }
+
+
+    // remove the remaining of the last row of the array in reverse
+    // 15,14,13, 10
     for (let k = squareArray.length - 1; k >= 0; k--) {
         arr.push(squareArray[squareArray.length - 1][k])
     }
     squareArray.pop();
 
+    // remove the first elements in the remaining rows
+    // 9, 5
     for (let l = squareArray.length - 1; l >= 0; l--) {
         arr.push(squareArray[l][0])
         squareArray[l].shift()
     }
 
+    // restart loops if any remaining
     if (squareArray.length > 0) {
         return unroll(squareArray, arr)
     }
